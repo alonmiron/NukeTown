b2d47fe3ac414ab07a151f064aa2cfd300436905
nadavkretzmer <kretzmer.nadav@gmail.com>
2023-03-05 23:07:18 +0200

    room 2.4

---
+++ Assets/MyAssets/Scripts/ClearBrowser.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ClearBrowser : MonoBehaviour
+{
+    public MeshRenderer screen1;
+    public MeshRenderer screen2;
+    public void OnClear()
+    {
+        screen1.gameObject.SetActive(false);
+        screen2.gameObject.SetActive(true);
+        
+    }
+
+}
